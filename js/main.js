96dc9a42cb118ff739946b2c847052cc26c6e027
aeeux <AndreasEriksen99@hotmail.com>
2021-03-24 18:30:34 +0100

    update
    update

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 
 
 //Smooth Scroll
+
 const body = document.body,
     scrollWrap = document.getElementsByClassName("smooth-scroll-wrapper")[0],
     height = scrollWrap.getBoundingClientRect().height - 1,
-    speed = 0.0765;
+    speed = 0.04;
 
 var offset = 0;
 
-body.style.height = Math.floor(height) + "px";
 
 function smoothScroll() {
     offset += (window.pageYOffset - offset) * speed;
