58cfc2722f7849a15b48e3db4a4459651262a26f
aeeux <AndreasEriksen99@hotmail.com>
2021-03-24 15:00:13 +0100

    update
    update

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+
+//Smooth Scroll
+const body = document.body,
+    scrollWrap = document.getElementsByClassName("smooth-scroll-wrapper")[0],
+    height = scrollWrap.getBoundingClientRect().height - 1,
+    speed = 0.04;
+
+var offset = 0;
+
+body.style.height = Math.floor(height) + "px";
+
+function smoothScroll() {
+    offset += (window.pageYOffset - offset) * speed;
+
+    var scroll = "translateY(-" + offset + "px) translateZ(0)";
+    scrollWrap.style.transform = scroll;
+
+    callScroll = requestAnimationFrame(smoothScroll);
+}
+
+smoothScroll();
