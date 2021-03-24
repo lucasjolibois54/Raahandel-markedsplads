2cc7bb9ae673441f5084e9e8c7a459f374a85447
aeeux <AndreasEriksen99@hotmail.com>
2021-03-24 16:52:51 +0100

    update
    update

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 const body = document.body,
     scrollWrap = document.getElementsByClassName("smooth-scroll-wrapper")[0],
     height = scrollWrap.getBoundingClientRect().height - 1,
-    speed = 0.04;
+    speed = 0.07;
 
 var offset = 0;
 
